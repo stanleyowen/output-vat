cffb1e9e7fdcb2eedcf785d5aedb0196a31ab9d8
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-06-17 19:38:40 +0700

    Save templates' path to system env

--- src/components/settings.components.tsx
+++ src/components/settings.components.tsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+
+const { ipcRenderer } = window.require("electron");
 
 const Settings = () => {
+  const [isLoading, setLoading] = useState<boolean>(false);
+
+  const UploadFile = () => {
+    setLoading(true);
+    const excelFileTemplate = document.getElementById(
+      "excel-file-template"
+    ) as any;
+    ipcRenderer.send(
+      "store-data",
+      JSON.stringify({
+        id: "excel-template",
+        value: excelFileTemplate.files[0].path,
+      })
+    );
+  };
+
   return (
     <div className="p-2">
       <h1 className="font-medium text-white text-lg mb-2">Settings</h1>
@@ -10,6 +28,7 @@ const Settings = () => {
           <span className="sr-only">Choose Excel Template</span>
           <input
             type="file"
+            id="excel-file-template"
             className="block w-full file:mr-4 file:py-1 file:px-4
             file:rounded-md file:border-0
             file:text-sm file:font-semibold
@@ -17,6 +36,12 @@ const Settings = () => {
             hover:file:bg-blue-500"
           />
         </label>
+        <button
+          onClick={() => UploadFile()}
+          className="block border-0 w-1/4 bg-slate-400/10 hover:bg-slate-400/20 text-white px-4 py-1 rounded-md"
+        >
+          Upload
+        </button>
       </div>
     </div>
   );
