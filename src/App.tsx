194e81d815b0646d7a01331c0fa9695444472e91
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-06-19 18:46:13 +0700

    Update Home Component Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import Home from "./components/home.components";
 import NavBar from "./components/navbar.components";
 import Settings from "./components/settings.components";
 
@@ -10,7 +11,7 @@ function App() {
     <HashRouter>
       <NavBar />
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
     </HashRouter>
