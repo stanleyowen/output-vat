a13f8369d40939aa5c818182d27045823be6cd18
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2024-04-28 21:45:15 +0700

    feat: able to open recently uploaded file template

--- src/components/settings.components.tsx
+++ src/components/settings.components.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import { openFilePath } from "../lib/file-operation.lib";
 import { Info, LoadingAnimate, OpenExternal } from "../lib/icons.lib";
 
 const { ipcRenderer } = window.require("electron");
 
 const Settings = () => {
-  const [isSuccess, setSuccess] = useState<boolean>(false);
+  const [path, setPath] = useState<string>("");
   const [isLoading, setLoading] = useState<boolean>(false);
+
+  const OpenOutputPath = () => openFilePath(path);
 
   const UploadFile = () => {
     setLoading(true);
@@ -24,7 +27,7 @@ const Settings = () => {
 
     ipcRenderer.once("store-data-status", (_: any, res: any) => {
       console.log(res);
-      setSuccess(true);
+      setPath(excelFileTemplate.files[0].path);
       setLoading(false);
     });
   };
@@ -33,21 +36,40 @@ const Settings = () => {
     <div className="p-2">
       <h1 className="font-medium text-white text-lg mb-2">Settings</h1>
 
-      {isSuccess && (
+      {path === "error" && (
         <div
           className="flex p-4 text-sm text-gray-700 bg-gray-100 rounded-lg dark:bg-gray-700 dark:text-gray-300 mb-3"
           role="alert"
         >
           <Info className="inline flex-shrink-0 mr-3 w-5 h-5" />
-          <div>
-            <span className="font-medium">Sucess!</span> File uploaded
-            sucessfully.
-          </div>
+          <span className="font-medium mr-1">Error!</span> Error in parsing
+          excel files. Please try again.
         </div>
       )}
 
+      {path !== "" && path !== "error" ? (
+        <div
+          className="flex p-4 text-sm text-gray-700 bg-gray-100 rounded-lg dark:bg-gray-700 dark:text-gray-300 mb-3"
+          role="alert"
+        >
+          <Info className="inline flex-shrink-0 mr-3 w-5 h-5" />
+          <span className="font-medium mr-1">Success!</span> Excel template
+          uploaded successfully.
+          <button
+            type="button"
+            className="ml-auto -mx-1.5 -my-1.5 bg-gray-100 text-gray-500 rounded-lg focus:ring-2 focus:ring-gray-400 p-1.5 hover:bg-gray-200 inline-flex h-8 w-8 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-800 dark:hover:text-white"
+            data-dismiss-target="#alert-border-5"
+            aria-label="Close"
+            onClick={() => OpenOutputPath()}
+          >
+            <span className="sr-only">Open in File Explorer</span>
+            <OpenExternal className="w-5 h-5" />
+          </button>
+        </div>
+      ) : null}
+
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <p className="my-auto">Excel template</p>
+        <p className="my-auto">Excel template (*.xlsx)</p>
         <label className="block">
           <span className="sr-only">Choose Excel Template</span>
           <input
